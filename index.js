355ac47de8a0a8b60879ccfa7940f938f5b35972
andrialiana <boursiere.liana@gmail.com>
2021-07-23 11:37:46 +0200

    Ajout des données

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let incomeField = document.getElementById('income_field');
 
-/*pour chaque periode
+/*
+données:
+- epargne (par periode)
+- periode (an, mois)
+- nbperiodestotal (pour limiter le nombre de lignes)
+- rendementperiode
+
+pour chaque periode:
 versementscumules = epargne X nbperiodes
 capitalperiode = totalperiode -1 + epargne
 interetsperiode = capitalperiode X rendementperiode
