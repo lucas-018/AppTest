c99cbed0708136f3228bc4edc54e975fc7a67fec
andrialiana <boursiere.liana@gmail.com>
2021-07-23 11:28:39 +0200

    Formules simples pour table

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 let incomeField = document.getElementById('income_field');
+
+/*pour chaque année
+versementscumules = epargne X nbperiodes
+capitalperiode = totalperiode -1 + epargne
+interetsperiode = capitalperiode X rendementperiode
+totalperiode = capitalperiode + intéretsperiode
+interetscumules = totalperiode - versementscumules
+
+*/
